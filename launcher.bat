1f7faef5ca6824372f69648ea637e41649373452
ZacJan <124002289+bablebab@users.noreply.github.com>
2023-09-30 23:39:17 +0200

    Add files via upload

---
+++ launcher.bat
@@ -0,0 +1,11 @@
+:: Lancer shiny.r à travers Rscript
+start Rscript shiny.r
+
+:: Installer les dépendances Python
+pip install -r requirements.txt
+
+:: Lancer dashboard.py
+start python dashboard.py
+
+echo Les applications sont lancées!
+pause
